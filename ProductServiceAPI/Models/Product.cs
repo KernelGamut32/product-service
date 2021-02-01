e07ff4e6fca74cd090dc9419e326dfbb490e3e8c
Allen Sanders <asanders@gamuttechnologysvcs.com>
2021-01-31 20:10:02 -0500

    Initial check-in

---
+++ ProductServiceAPI/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductServiceAPI.Models
+{
+    public enum InventoryStatus
+    {
+        Stocked,
+        Low,
+        Critical,
+        Back_Order
+    }
+
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string CatalogNumber { get; set; }
+        public string Description { get; set; }
+        public decimal UnitCost { get; set; }
+        public int QuantityInStock { get; set; }
+        public InventoryStatus InventoryStatus { get; set; }
+    }
+}
